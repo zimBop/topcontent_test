94b3da18b4f4f4d46315b1b88bc32cd3229745cf
ZimBop <miterkr@gmail.com>
2020-12-28 12:38:14 +0300

    Update ArtisanSeeder

--- database/seeds/ArtisanSeeder.php
+++ database/seeds/ArtisanSeeder.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Artisan;
+use App\Models\WorkDay;
 
 class ArtisanSeeder extends Seeder
 {
+    protected $workDays;
+
+    public function __construct()
+    {
+        $this->workDays = WorkDay::where(WorkDay::DATE, '>=', now()
+            ->format('Y-m-d'))->get();
+    }
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +21,27 @@ class ArtisanSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\Artisan::class, 20)->create();
+        factory(Artisan::class, 20)->create()
+            ->each(fn($artisan) => $this->createSchedule($artisan));
+    }
+
+    protected function createSchedule(Artisan $artisan)
+    {
+        $this->workDays->each(function ($workDay) use ($artisan) {
+            if ($this->isNotWorkingDay()) {
+                return;
+            }
+
+            $artisan->work_days()->attach($workDay->id);
+        });
+    }
+
+    /**
+     * 30% chance the day is not working
+     * 70% chance the day is working
+     */
+    protected function isNotWorkingDay()
+    {
+        return rand(1, 100) > 70;
     }
 }
